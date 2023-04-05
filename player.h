72992ceaa1f20ded8c7c9847375c6c7628b79b5c
Nandish <nandish.patel@uwaterloo.ca>
2023-04-05 19:03:18 -0400

    Updating player.h

--- player.h
+++ player.h
@@ -1,8 +1,14 @@
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
 
+#include "square.h"
+#include "enumerations.h"
+
 #include <vector>
 #include <map>
+
+using std::vector;
+using std::map;
 
 class Player {
     int cur_pos;
