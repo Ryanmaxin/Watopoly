86102c297a686aa658f8f0f5e9c4d44898859824
Nandish <nandish.patel@uwaterloo.ca>
2023-04-06 15:48:40 -0400

    Adding the implementaion for Dice files

--- dice.h
+++ dice.h
@@ -1,5 +1,6 @@
 #ifndef __DICE_H__
 #define __DICE_H__
+
 #include <string>
 #include "square.h"
 #include "player.h"
@@ -11,9 +12,13 @@ class Dice {
     int die1;
     int die2;
     int doubles_rolled;
+    int rollDie1();
+    int rollDie2();
+    Dice();
     public:
         int rollDice();
         bool isDoubles();
         bool threeDoubles();
 };
+
 #endif
