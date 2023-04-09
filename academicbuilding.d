611343c4893b3de691cb5062826810391e25d41a
Nandish <nandish.patel@uwaterloo.ca>
2023-04-08 21:53:18 -0400

    Updated controller.cc

--- academicbuilding.d
+++
@@ -1,4 +0,0 @@
-academicbuilding.o: academicbuilding.cc academicbuilding.h \
-  ownableproperty.h square.h enumerations.h player.h board.h gym.h \
-  dice.h residence.h unownableproperty.h response.h trade.h subject.h \
-  observer.h
